5fbea273e8fa43b3780c43f3423647a9053e0d32
Nathany Mendonça <69328762+nathanymendonca@users.noreply.github.com>
2021-01-31 21:20:07 -0300

    criação do servidor e calculadora usando python

---
+++ api.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__, template_folder="./src/views")
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if (request.method =="GET"):
+        return render_template("index.html")
+    else:
+        return "Você está acenssando o seu gringoBank's"    
+
+
+@app.errorhandler(404) 
+def not_found(error):
+        return render_template("error.html")   
+
+app.run(port=934, debug=True)    
